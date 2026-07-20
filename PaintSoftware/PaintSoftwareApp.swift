dc479b60afae44514e20c8ffa50893d2860df8a0
Kevin Park <kevinpark0807@gmail.com>
2026-07-19 22:21:55 -0400

    Initial Commit

---
+++ PaintSoftware/PaintSoftwareApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintSoftwareApp.swift
+//  PaintSoftware
+//
+//  Created by Julia Park on 2026-07-19.
+//
+
+import SwiftUI
+
+@main
+struct PaintSoftwareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
